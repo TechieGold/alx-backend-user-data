b4eefca1828fa813ddeb57200a665fa52019d53b
Gold Israel <techiegold101@gmail.com>
2024-04-18 13:43:37 +0100

    add basic authentication class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module contains a class BasicAuth that inherits from Auth.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    This class Provides basic authentication-related functionality for the API.
+    It inherits from Auth.
+    """
+    pass
