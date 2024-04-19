f0c0f08070545be652fd0e8afd3790b539507869
Gold Israel <techiegold101@gmail.com>
2024-04-19 05:59:04 +0100

    add method to get userID associated with a session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,12 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return (session_id)
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Get the User ID associated with a Session ID. """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        
+        user_id = self.user_id_by_session_id.get(session_id)
+        
+        return (user_id)
