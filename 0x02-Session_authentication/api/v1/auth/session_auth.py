5e1ef6ed3ab1fa6658f28a30fc65f1086046f769
Gold Israel <techiegold101@gmail.com>
2024-04-19 05:04:39 +0100

    setup class for authentication machnisim

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module contains a class SessionAuth that inherits from Auth.
+"""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """
+    This class Provides basic authentication-related functionality for the API.
+    It inherits from Auth.
+    """
+    pass
