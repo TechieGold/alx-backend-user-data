2d9e9f193f9cc823c43f8a57a87fa6c785fc335a
Gold Israel <techiegold101@gmail.com>
2024-04-27 07:37:35 +0100

    fix pycodestyle error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db",
+                                     echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -30,6 +31,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Create a User object and save it to the database
