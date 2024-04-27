56b8212ae7d32cc29c58281dea743411a0eaa102
Gold Israel <techiegold101@gmail.com>
2024-04-27 09:14:46 +0100

    add users view

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,31 @@
 """
 Baic Flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+Auth = Auth()
 
 
 @app.route("/")
-def hello():
+def home():
     """ Route handler for the root endpoint """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """ Routes that register new users"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = Auth.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
