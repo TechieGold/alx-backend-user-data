b7b23f27a957cb838bcd1d2a1c420d231f93a732
Gold Israel <techiegold101@gmail.com>
2024-04-29 09:04:28 +0100

    simplify code, same funtionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,5 +80,8 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ Destroy the session for the user """
 
-        user = self._db.find_user_by(id=user_id)
-        self._db.update_user(user, session_id=None)
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
+        return None
