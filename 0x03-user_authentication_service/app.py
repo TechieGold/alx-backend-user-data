b3c7668ea275af2164a4bd7ba063697fb26dba1f
Gold Israel <techiegold101@gmail.com>
2024-04-29 21:35:43 +0100

    use f-string to print payload

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,9 +97,10 @@ def update_password():
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+
+    return jsonify({"email": f"{email}", "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
